82ab90d9c16961faae7b7a4d5ac1ef149f1ea943
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-16 15:05:30 +0700

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//component 
+import ProtectedRoute from "./auth/ProtectedRoute";
+import LoadingPage from './components/LoadingPage';
+import LoginPage from './pages/LoginPage';
+
+//pages
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const Profile = React.lazy(() => import('./pages/Profile'));
+const Setting = React.lazy(() => import('./pages/Setting'));
 
 const App = () => {
   return (
-    <div>
-     
-    </div>
+    <React.Suspense fallback={<LoadingPage/>}>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/">
+            <Route index element={<HomePage />} />
+            <Route path="login" element={<LoginPage/>} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="me">
+              <Route index element={<Profile />} />
+              <Route path="setting" element={<Setting />} />
+            </Route>
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
+    </React.Suspense>
   );
 }
 
