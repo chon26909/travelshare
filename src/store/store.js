3b1b4c3403b2d2917acd843c572cb3792af68488
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-20 23:39:09 +0700

    config store

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+//slices
+import authReducer from "./slices/users/auth";
+
+const reducers = { 
+    auth: authReducer
+}
+
+export const store = configureStore({
+    reducer: reducers,
+    devTools:false
+});
