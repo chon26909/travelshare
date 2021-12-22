868bcf53ff18d273a9bf8fa49c8ecd6e45c1688b
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-22 21:32:09 +0700

    edit login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,37 +1,56 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import "../styles/login.css"
+import { Container, Form, Button, Header } from 'semantic-ui-react';
 
 const LoginPage = () => {
 
     const email = useRef(null);
     const password = useRef(null);
 
+    const [loadingLogin, setloadingLogin] = useState(false)
+
     const submitLogin = (e) => {
         e.preventDefault();
-        console.log("email",email.current.value);
-        console.log("password",password.current.value);
+        console.log("email", email.current.value);
+        console.log("password", password.current.value);
+        setloadingLogin(true)
     }
 
     return (
-        <div className='container vh-100'>
-            {/* <h1 className='pt-5 text-center'>ลงชื่อเข้าใช้</h1>
-            <Form className='form-login mx-auto pt-3' onSubmit={(e) => submitLogin(e)}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>อีเมล</Form.Label>
-                    <Form.Control type="email" ref={email} placeholder="xxxxx@gmail.com" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>รหัสผ่าน</Form.Label>
-                    <Form.Control type="password" ref={password} placeholder="********" />
-                </Form.Group>
-                <div className='d-grid'>
-                    <Button variant="primary" type="submit">
-                        ลงชื่อเข้าใช้
-                    </Button>
-                </div>
-            </Form> */}
+        <div className='login-page'>
+            <Container text>
+            <Header as='h1'>Login </Header>
+                <Form size='big'  onSubmit={submitLogin}>
+                    {/* <Form.Field required={true}>
+                        <label>Email</label>
+                        <input type='email' placeholder='xxxxX@gmail.com' required/>
+                    </Form.Field>
+                    <Form.Field required={true}>
+                        <label>Password</label>
+                        <input type='password' placeholder='********' required/>
+                    </Form.Field> */}
+                    <Form.Input
+                        icon='mail'
+                        iconPosition='left'
+                        label='Username'
+                        placeholder='xxxxx@gmail.com'
+                        required={true}
+                        input={email}
+                    />
+                    <Form.Input
+                        icon='lock'
+                        iconPosition='left'
+                        label='Password'
+                        type='password'
+                        placeholder='xxxxxxxx'
+                        required={true}
+                        input={password}
+                    />
+                    <Button loading={loadingLogin} type='submit' fluid size='big' color='violet'>Login</Button>
+                </Form>
+            </Container>
         </div>
+
     )
 }
 
