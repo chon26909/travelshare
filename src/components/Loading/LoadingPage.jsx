6c5b48bdb0a9f4b69159e77ab34092a3071a4a4d
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-26 22:26:03 +0700

    loading page

--- src/components/Loading/LoadingPage.jsx
+++ src/components/Loading/LoadingPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
+import spinner from '../../assets/images/loading/Ball.gif'
+import './loading.css'
 
 const LoadingPage = () => {
     return (
         <div>
             <Container>
-                
+                <div className='spinner-center'>
+                    <img src={spinner} alt="loading page"  />
+                </div>
             </Container>
         </div>
     )
