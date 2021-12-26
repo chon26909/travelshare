9a39f355de43d34f0c8997a106c56d1cdc9055c0
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-26 22:06:30 +0700

    edit file

---
+++ src/components/Loading/LoadingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const LoadingPage = () => {
+    return (
+        <div>
+            <Container>
+                
+            </Container>
+        </div>
+    )
+}
+
+export default LoadingPage
