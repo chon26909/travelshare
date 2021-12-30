b97fcc26fd9fbaedd6a7a7aaabac03bab9ff58c0
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-30 14:17:22 +0700

    update component search

---
+++ src/components/Landing/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './search.css'
+
+export const Search = () => {
+    return (
+        <div>
+            <div className='overlay-search'>
+
+            </div>
+            <img className='bg-search' src="https://2ppost.com/wp-content/uploads/2021/04/travel-world.jpg"/>
+        </div>
+    )
+}
